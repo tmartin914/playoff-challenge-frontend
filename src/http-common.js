723c61e1686bf8483c41c382c519551fbe989080
Tyler Martin <tmartin024@gmail.com>
2024-01-22 20:22:37 -0500

    local dev env var

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: "http://localhost:8080/api", // local
-  baseURL: "https://playoff-challenge-backend-d170a30e6d94.herokuapp.com/api", // deploy
+  //baseURL: process.env.LOCAL_DEV ? "http://localhost:8080/api" : "https://playoff-challenge-backend-d170a30e6d94.herokuapp.com/api",
+  baseURL: "http://localhost:8080/api", // local
+  //baseURL: "https://playoff-challenge-backend-d170a30e6d94.herokuapp.com/api", // deploy
   headers: {
     "Content-type": "application/json"
   }
